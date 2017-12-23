d7d778a9331a36770fc711e292d4b62f5bbb6747
Simone Celia <simon387@hotmail.it>
2017-12-24 00:42:18 +0100

    primo_abbozzo_download_fisso_ok

--- src/main/java/ytdlweb/service/impl/YytdlwebService.java
+++ src/main/java/ytdlweb/service/impl/YytdlwebService.java
@@ -4,11 +4,44 @@ import org.springframework.stereotype.Service;
 import ytdlweb.model.Mp3;
 import ytdlweb.service.IYtdlwebService;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @Service
 public class YytdlwebService implements IYtdlwebService {
 
 	@Override
 	public Mp3 downloadMp3(String url) {
+		url = "https://www.youtube.com/watch?v=oHNKTlz1lps";
+
+		String command = "youtube-dl -x -o /tmp/%(title)s-%(id)s.%(ext)s " + url;
+
+		String output = executeCommand(command);
+
+
 		return null;
+	}
+
+	private String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process process;
+
+		try {
+			process = Runtime.getRuntime().exec(command);
+			process.waitFor();
+			BufferedReader bufferedReader =
+					new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				output.append(line + "\n");
+				System.out.print(output);
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return output.toString();
 	}
 }
