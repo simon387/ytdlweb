d90b501edc8a213d86e45fb6d7c4d8fc86c00550
Simone Celia <simon387@hotmail.it>
2018-01-14 17:58:00 +0100

    fix conflitti

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import ytdlweb.rest.YtdlwebRestController;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class ApplicationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = standaloneSetup(new YtdlwebRestController()).build();
+	}
+
+//	@Test
+//	public void testYtdlwebRestController() throws Exception {
+//
+//		String json = "{\"url\": \"https://www.youtube.com/watch?v=oHNKTlz1lps\",\"type\": \"mp3\"}";
+//		this.mockMvc.perform(post("/ytdl").accept(MediaType.parseMediaType("application/json;charset=UTF-8")).)
+//				;
+//	}
+
+	@Test
+	public void testYtdlwebRestController() throws Exception {
+
+		String json = "{\"url\": \"https://www.youtube.com/watch?v=oHNKTlz1lps\",\"type\": \"mp3\"}";
+
+		this.mockMvc.perform(post("/ytdl")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+				.andExpect(status().isCreated());
+	}
+}
